a78b831e0a9cde14a5b7f78e8945529b892696aa
Angela Stephens <angelastephens317@gmail.com>
2020-06-10 01:02:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,11 @@
 # Your code goes here!
 class Dog
   fido=Dog.new
-def fido.name= ("Fido")
-  @fido.name
+  def fido.name= ("Fido")
+    @fido.name
+  end
+  def fido.bark
+    puts woof!
+  end
+end
+
